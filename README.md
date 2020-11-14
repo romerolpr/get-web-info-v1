77852f9ca623e5c4eb633f59e4c527f8c467fa17
Lucas Romero <lucas.romero@doutoresdaweb.com.br>
2020-11-13 22:27:22 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Bem vindo ao GetWebInfo v1.0 | Welcome to the GetWebInfo v1.0
+
+## Configuração e preparação do ambiente | Let's prepare and configure the machine
+
+## PT-BR
+* Instale o Python na sua máquina.
+* Instale todas as bibliotecas utilizando o comando "pip install" no cmd. 
+
+## EN
+* Install Python on your machine
+* Install all packages using the comand "pip install" on cmd
+
+## Pacotes para instalar | Packages to install
+
+```bash
+pip install beautifulsoup4
+pip install tqdm
+pip install colorama 
+```
---
+++ validador.py
@@ -0,0 +1,254 @@
+# Definir idioma | set language
+# PT = 0
+# EN = 1
+
+lang = 0
+
+from tqdm.auto import tqdm
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from colorama import Fore, Style
+import threading
+import re
+import socket
+
+class Validador:
+
+	def __init__(self, lang=0):
+		self.lang = lang
+		self.path = 'Resultados'
+		self.servidores = [
+			'172.217.28.78',
+			'157.240.222.35',
+			'104.244.45.65',
+			'172.217.162.110',
+			'35.172.167.44',
+		]
+
+	def Set_language(self, lang, config=False):
+
+		Sitemap = [
+			'Fora do servidor'
+			'Sitemap correto', 
+			'Sitemap incorreto', 
+			'Sitemap inexistente',
+			'Sitemap não recuperado',
+			'Não recuperado'
+		] if lang == 0 else [
+			'Off the server', 
+			'Sitemap correct', 
+			'Sitemap invalid', 
+			'Sitemap nonexistent',
+			'Sitemap not recovered',
+			'Not recovered'
+		]
+
+		Tag = [
+			'Fora do servidor', 
+			'Tag recuperada', 
+			'Tag não recuperada',
+			'Cerificado inválido',
+			'Domínio congelado'
+		] if lang == 0 else [
+			'Off the server', 
+			'Tag recovered', 
+			'Tag not recovered',
+			'Certificate invalid',
+			'Not recovered'
+		]
+
+		Servidor = [
+			'Fora do servidor', 
+			'Dentro do servidor', 
+			'Não recuperado'
+		] if lang == 0 else [
+			'Off the server', 
+			'Inside the server', 
+			'Not recovered'
+		]
+
+		SSL = [
+			'Fora do servidor', 
+			'SSL ativo', 
+			'SSL não ativo'
+			'Não recuperado'
+		] if lang == 0 else [
+			'Off the server', 
+			'SSL active', 
+			'SSL not active', 
+			'Not recovered'
+		]
+		
+		return {
+			'sitemap'	: Sitemap,
+			'tag'		: Tag,
+			'servidor'	: Servidor,
+			'ssl'		: SSL	
+		}[config]
+
+	def Base(self, url, www=True):
+		try:
+			url = url.split('//')
+			url = url[1].split('/')
+			if www:
+				return url[0]
+			else:
+				return url[0].replace('www.', '')
+		except:
+			return url[0]
+
+	def Saiu(self, url, test = False):
+		IP = socket.gethostbyname(self.Base(url))
+		if IP not in self.servidores:
+			return IP
+		else:
+			return False if not test else IP
+
+	def Tag(self, url):
+		Def = 'tag'
+		try:
+			if self.Saiu(url):
+				with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[0] + '.txt', "a", -1, encoding='utf-8') as file:
+					file.write('{} => {}\n'.format(url, self.Saiu(url)))
+			else:
+				try:
+					request = urlopen(f"https://{url}/").read()
+					html = BeautifulSoup(request, "html5lib");
+					try:
+						script = re.search(r"ga\('(create)',\s+'(.*?)',(.*?)\s+'(.*?)'\);", str(html.body)).group(2)
+						with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[1] + '.txt', 'a', encoding='utf-8') as file:
+							file.write(f'{url}: {script}\n')
+					except:
+						try:
+							script = re.search(r"gtag\([\"\']config[\"\'].*?[\"\'](.*?)[\"\']\);", str(html.body)).group(1)
+							with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[1] + '.txt', 'a', encoding='utf-8') as file:
+								file.write(f'{url}: {script}\n')
+						except:
+							with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[2] + '.txt', 'a', encoding='utf-8') as file:
+								file.write(f'{url}\n')
+				except:
+					with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[3] + '.txt', 'a', encoding='utf-8') as file:
+						file.write(f'{url}\n')
+		except:
+			with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[4] + '.txt', 'a', encoding='utf-8') as file:
+				file.write(f'{url}\n')
+
+	def Servidor(self, url):
+		Def = 'servidor'
+		try:
+			if self.Saiu(url):
+				with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[0] + '.txt', "a", -1, encoding='utf-8') as file:
+					file.write('{} => {}\n'.format(url, self.Saiu(url)))
+			else:
+				with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[1] + '.txt', "a", -1, encoding='utf-8') as file:
+					file.write(f'{url} => {self.Saiu(url, test=True)}\n')
+		except:
+			with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[2] + '.txt', 'a', encoding='utf-8') as file:
+				file.write(f'{url}\n')
+
+	def Certificado_SSL(self, url):
+		Def = 'ssl'
+		try:
+			if self.Saiu(url):
+				with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[0] + '.txt', "a", -1, encoding='utf-8') as file:
+					file.write('{} => {}\n'.format(url, self.Saiu(url)))
+			else:
+				try:
+					request = urlopen(f"https://www.{url}/").read()
+					with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[1] + '.txt', "a", -1, encoding='utf-8') as file:
+						file.write(f'{url}\n')
+				except:
+					with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[2] + '.txt', "a", -1, encoding='utf-8') as file:
+						file.write(f'{url}\n')
+		except:
+			with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[3] + '.txt', 'a', encoding='utf-8') as file:
+				file.write(f'{url}\n')
+
+	def Sitemap(self, url):
+
+		Def = 'sitemap'
+
+		http = [ f'https://www.{url}/', f'https://{url}', f'http://{url}', f'http://www.{url}/' ]
+
+		try:
+			if self.Saiu(url):
+				with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[0] + '.txt', "a", -1, encoding='utf-8') as file:
+					file.write('{} => {}\n'.format(url, self.Saiu(url)))
+			else:
+				try:
+
+					request = urlopen('{}sitemap.xml'.format(http[0])).read()
+					html = BeautifulSoup(request, "html5lib")
+
+					sitemap = [
+						re.findall(http[0], str(html.body)),
+						re.findall(http[1], str(html.body)),
+						re.findall(http[2], str(html.body)),
+						re.findall(http[3], str(html.body)),
+					]
+
+					if sitemap[0]:
+						if len(sitemap[0]) in range(20, 10001):
+							with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[1] + '.txt', "a", -1, encoding='utf-8') as arquivo:
+								arquivo.write(f'{url} => https com www\n')
+						else:
+							with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[2] + '.txt', "a", -1, encoding='utf-8') as arquivo:
+								arquivo.write(f'{url}\n')
+						
+					elif sitemap[1]:
+						if len(sitemap[1]) in range(20, 10001):
+							with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[1] + '.txt', "a", -1, encoding='utf-8') as arquivo:
+								arquivo.write(f'{url} => https sem www\n')
+						else:
+							with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[2] + '.txt', "a", -1, encoding='utf-8') as arquivo:
+								arquivo.write(f'{url}\n')
+							
+					elif sitemap[2]:
+						if len(sitemap[2]) in range(20, 10001):
+							with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[1] + '.txt', "a", -1, encoding='utf-8') as arquivo:
+								arquivo.write(f'{url} => http sem www\n')
+						else:
+							with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[2] + '.txt', "a", -1, encoding='utf-8') as arquivo:
+								arquivo.write(f'{url}\n')
+								
+					elif sitemap[3]:
+						if len(sitemap[3]) in range(20, 10001):
+							with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[1] + '.txt', "a", -1, encoding='utf-8') as arquivo:
+								arquivo.write(f'{url} => http com www\n')
+						else:
+							with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[2] + '.txt', "a", -1, encoding='utf-8') as arquivo:
+								arquivo.write(f'{url}\n')
+					else:
+						with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[3] + '.txt', 'a', encoding='utf-8') as file:
+							file.write(f'{url}\n')
+
+				except:
+					with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[4] + '.txt', 'a', encoding='utf-8') as file:
+						file.write(f'{url}\n')
+		except:
+			with open(self.path + f'/{Def}/' + self.Set_language(0, Def)[5] + '.txt', 'a', encoding='utf-8') as file:
+				file.write(f'{url}\n')
+
+validador = Validador(lang)
+
+def crawler(arquivo):  
+	try:
+		with open(f"{arquivo}.txt", "r", encoding='utf-8') as sites:
+			linha = sites.readlines()
+			arrayUrl = []
+			for i in linha:
+				arrayUrl.append(i.strip("\n").strip(" "))
+
+			for url in tqdm(arrayUrl, desc="Validando os links"):
+				validador.Sitemap(url)
+				# threading.Thread(
+				#     target=validador.Tag,
+				#     args=(url,)).start()
+		return True
+	except:
+		return False
+
+if crawler('sites'):
+	print('\nEscrevendo dados...' if lang == 0 else '\nWriting data...')
+else:
+	input('\nFalha.' if lang == 0 else '\nFail.')
